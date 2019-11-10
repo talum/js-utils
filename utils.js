7831e0d9f394b59f751cc508af223b1328a40a48
talum <tracy.a.lum@gmail.com>
2019-11-09 21:20:03 -0500

    some more utils

--- utils.js
+++ utils.js
@@ -51,23 +51,105 @@ function filter(array, condition) {
   return results;
 }
 
-console.log('myFilter');
+//console.log('myFilter');
 //const myFilter = filter(myArray, (el) => {
   //return el % 2 == 0;
 //});
-const myFilter = filter(myArray, 2);
-console.log(myFilter);
+//const myFilter = filter(myArray, 2);
+//console.log(myFilter);
 
-function every() {
+function every(array, condition) {
+  // return true or false
+  for(let i = 0; i < array.length; i++) {
+    if (!condition(array[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function shuffle() {
 }
 
-function throttle() {
+function throttle(func, wait) {
+  var timeout;
+  var args;
+  var previousCall = 0;
+  var context;
+
+  let callLater = function () {
+    result = func.apply(context, args);
+    previousCall = Date.now();
+    timeout = null;
+  }
+
+  return function() {
+    let now = Date.now();
+    let remaining = wait - (now - previousCall);
+    context = this;
+    args = arguments;
+
+    if (!previousCall) {
+      result = func.apply(this, arguments);
+      previousCall = Date.now();
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+    } else if (remaining <= 0) {
+      result = func.apply(this, arguments);
+      previousCall = Date.now();
+    } else {
+      timeout = setTimeout(later, remaining)
+    }
+    return result;
+    // if never previously called
+    // call the function and set the previous call
+    // if previously called and remaining wait time < 0
+    // call the function and set the previous call
+    // if previously called and remaining wait time > 0
+    // call the function after an interval && reset the interval after
+  }
+
+  // when invoked repeatedly, only calls function at most once per every
+  // wait milliseconds
+  // then as soon as the wait period is over
+}
+const sayThings = function(word1, word2) {
+  console.log(word1);
+  console.log(word2);
 }
 
-function debounce() {
-}
+const throttledSayThings = throttle(sayThings, 10000);
+throttledSayThings('hello', 'world')
+throttledSayThings('hello2', 'world2')
+throttledSayThings('hello3', 'world3')
 
+
+function debounce(func, wait) {
+  // postpone execution until after wait milliseconds have elapsed since the
+  // last time it was invoked
+  // will call most recently invoked args
+  let timeout = null;
+  let context;
+  let result;
+  let funcArgs;
+
+  const later = function later() {
+    timeout = null;
+    result = func.apply(context, args);
+  }
+
+  return function(...args) {
+    context = this;
+    funcArgs = args;
+    if (!timeout) {
+      timeout = setTimeout(later, wait);
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    }
+    return result;
+  }
+}
 

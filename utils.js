6aee6c4c47a3deb6ccce3cbee02f8f6e98e56668
talum <tracy@flatironschool.com>
2019-11-28 12:04:27 -0500

    add retry

--- utils.js
+++ utils.js
@@ -61,7 +61,7 @@ function filter(array, condition) {
 function every(array, condition) {
   // return true or false
   for(let i = 0; i < array.length; i++) {
-    if (!condition(array[i]) {
+    if (!condition(array[i])) {
       return false;
     }
   }
@@ -120,10 +120,10 @@ const sayThings = function(word1, word2) {
   console.log(word2);
 }
 
-const throttledSayThings = throttle(sayThings, 10000);
-throttledSayThings('hello', 'world')
-throttledSayThings('hello2', 'world2')
-throttledSayThings('hello3', 'world3')
+//const throttledSayThings = throttle(sayThings, 10000);
+//throttledSayThings('hello', 'world')
+//throttledSayThings('hello2', 'world2')
+//throttledSayThings('hello3', 'world3')
 
 
 function debounce(func, wait) {
@@ -152,4 +152,60 @@ function debounce(func, wait) {
     return result;
   }
 }
+
+function retryWithTimeout(func,maxRetry,wait) {
+  // retry function retryCount number of times with a timeout of timeout
+  // wrap the function in a promise?
+  let retryCount = 0;
+  let context;
+  let timeout;
+  let funcArgs;
+
+  function callFunction() {
+    console.log('calling function')
+    try {
+      if (timeout) { clearTimeout(timeout); }
+
+      func.apply(context, funcArgs)
+      .then((resp) => console.log(resp))
+      .catch((e) => {
+        if (retryCount < maxRetry) {
+          timeout = setTimeout(callFunction, wait)
+          retryCount++;
+        } else {
+          console.log(e)
+          return e;
+        }
+      })
+    } catch (e) {
+      if (retryCount < maxRetry) {
+        timeout = setTimeout(callFunction, wait)
+        retryCount++;
+      } else {
+        console.log(e)
+        return e;
+      }
+    }
+  }
+
+  return function(...args) {
+    context = this;
+    funcArgs = args;
+
+    callFunction();
+  }
+}
+
+function fetch() {
+  return new Promise((resolve, reject) => {
+    if ((Math.floor(Math.random() * 100) % 2) == 0) {
+      resolve('hello');
+    } else {
+      reject('oops');
+    }
+  })
+}
+
+let retriable = retryWithTimeout(fetch, 3, 1000)
+retriable('http://www.google.com');
 
